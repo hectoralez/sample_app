6477078ad868ace35e332977ca0ea9e41b67feec
Hector <hector@hingeto.com>
2018-09-28 20:18:00 -0700

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,7 +4,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "layout links" do
+  def setup
+    @user = users(:hector)
+  end
+  test "layout links for logged-in and non-logged-in users" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -15,5 +18,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get contact_path
     assert_select "title", full_title("Contact")
+
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", "#", text: "Account"
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+
   end
 end
